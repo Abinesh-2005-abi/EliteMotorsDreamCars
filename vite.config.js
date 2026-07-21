de2ea463c9c0b20450d5677bbd3bc60a7346e8e7
Abinesh-2005-abi <abiabineshrs00@gmail.com>
2026-07-21 12:27:49 +0530

    Fixed Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/EliteMotorsCars/",
-});
-=======
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/EliteMotorsCars/',
+  base: '/',
 })
->>>>>>> a3539c5bf16a18967770394632bc36b33b06d83e
